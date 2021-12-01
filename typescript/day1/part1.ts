5ccbb2c72ae7f79dfe3ef539f34248b0eca14dc0
charlesworth <cochrane26@gmail.com>
2021-12-01 10:39:55 +0000

    added day1 part1 solution

--- typescript/day1/part1.ts
+++ typescript/day1/part1.ts
@@ -2,5 +2,15 @@ import { readFileSync } from 'fs';
 
 console.log('Advent of code: day 1, part 1');
 
-const file = readFileSync('./inputs/day1/part1', 'utf-8');
-console.log(file);
+const depths = readFileSync('./inputs/day1/part1', 'utf-8').split("\n").map(strValue => Number.parseInt(strValue, 10));
+
+let depthIncreaseCount = 0;
+for (let i = 1; i < depths.length; i++) {
+    const currentDepth = depths[i];
+    const previousDepth = depths[i-1];
+    if (currentDepth > previousDepth) {
+        depthIncreaseCount += 1;
+    }
+}
+
+console.log(`Answer: ${depthIncreaseCount}`);
