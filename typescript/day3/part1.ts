4f598c395d219456ca40261631e5c7d9fb3b3e62
charlesworth <cochrane26@gmail.com>
2021-12-03 10:31:39 +0000

    added day3 part1 solution

---
+++ typescript/day3/part1.ts
@@ -0,0 +1,28 @@
+import { readFileSync } from 'fs';
+
+console.log('Advent of code: day 3, part 1');
+
+const binaryLines: string[] = readFileSync('./inputs/day3/part1', 'utf-8').split("\n");
+    
+let cumulativeOnesPerColumn: number[] = [0,0,0,0,0,0,0,0,0,0,0,0];
+binaryLines.forEach(binaryLine => {
+    [...binaryLine].forEach((binaryChar, i) => {
+        if (binaryChar == "1") cumulativeOnesPerColumn[i] += 1;
+    });
+});
+
+let gammaRateStr: string = "";
+let epsilonRateStr: string = "";
+cumulativeOnesPerColumn.forEach((cumulativeOnes) => {
+    if (cumulativeOnes > (binaryLines.length / 2)) {
+        gammaRateStr += "1";
+        epsilonRateStr += "0";
+    } else {
+        gammaRateStr += "0";
+        epsilonRateStr += "1";
+    }
+});
+const gammaRate: number = Number.parseInt(gammaRateStr, 2)
+const epsilonRate: number = Number.parseInt(epsilonRateStr, 2);
+
+console.log(`Answer: ${gammaRate * epsilonRate}`);
