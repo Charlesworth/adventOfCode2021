33f79663dfc65d5eb2fa6bf35eb7f67f0dd83a5a
charlesworth <cochrane26@gmail.com>
2021-12-08 08:43:52 +0000

    added day8 part1 solution

---
+++ typescript/day8/part1.ts
@@ -0,0 +1,29 @@
+import { readFileSync } from 'fs';
+
+console.log('Advent of code: day 8, part 1');
+
+const outputValues = readFileSync('./inputs/day8/part1', 'utf-8').split("\n").map(line => line.split(" | ")[1].split(" ")).flat();
+
+const [lengthOf1, lengthOf4, lengthOf7, lengthOf8] = [2, 4, 3, 7];
+let [amountOf1, amountOf4, amountOf7, amountOf8] = [0, 0, 0, 0];
+
+outputValues.forEach(outputValue => {
+        switch (outputValue.length) {
+            case lengthOf1:
+                amountOf1++;
+                return;
+            case lengthOf4:
+                amountOf4++;
+                return;
+            case lengthOf7:
+                amountOf7++;
+                return;
+            case lengthOf8:
+                amountOf8++;
+                return;
+            default:
+                break;
+        }
+    })
+
+console.log(`Number of 1s, 4s, 7s, or 8s: ${amountOf1 + amountOf4 + amountOf7 + amountOf8}`);
